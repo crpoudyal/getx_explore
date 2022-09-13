962c7dd038f26e157f3e34dff2fa2abd5ad83e1f
crpoudyal <crpoudyal@gmail.com>
2022-09-13 21:12:22 +0545

    fix: debugcheckedbanner removed

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
